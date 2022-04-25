56a1619c87e1a3704827771fab0781a91dffc16f
anupamsukla <32832216+anupamsukla@users.noreply.github.com>
2022-04-25 14:08:32 +0530

    FIRST UPDATE

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const { handleAuth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  return (
+    <form
+      className="loginform"
+      onSubmit={(e) => {
+        handleAuth(true);
+        navigate(-1, { replace: true });
+        e.preventDefault();
+      }}
+    >
+      <input
+        name="username"
+        type="text"
+        placeholder="Enter username"
+        className="login_username"
+      />
+      <input
+        name="password"
+        type="text"
+        placeholder="Enter password"
+        className="login_password"
+      />
+      <input type="submit" value="SIGN IN" className="login_submit" />
+    </form>
+  );
+};
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Link className="nav-home" to="/">
+        Home
+      </Link>
+      <Link className="nav-list" to="/employees">
+        Employee List
+      </Link>
+      <Link className="nav-admin" to="/admin">
+        Admin
+      </Link>
+      {/* Show Either logout or login based on auth context. DO NOT show both */}
+      <Link className="nav-logout" to="/logout">
+        Logout
+      </Link>
+
+      <Link className="nav-login" to="/login">
+        Login
+      </Link>
+    </div>
+  );
+};
